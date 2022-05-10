f3db84068d73c8f16f45dc14830772c06c1ffc44
Val Subaan <rindelProgrammer2021@gmail.com>
2022-05-10 23:13:37 +0800

    add: general settings

---
+++ nvim/.config/nvim/lua/settings/init.lua
@@ -0,0 +1,27 @@
+-- general settings
+local set = vim.opt
+
+set.expandtab = true
+set.smarttab = true
+
+set.shiftwidth = 4
+set.tabstop = 4
+set.softtabstop = 4
+
+set.number = true
+set.relativenumber = true
+
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.splitbelow = true
+set.splitright = true
+set.wrap = false
+set.fileencoding = 'utf-8'
+set.guifont = 'Hack'
+set.termguicolors = true
+
+set.smartindent = true
+set.autoindent = true
