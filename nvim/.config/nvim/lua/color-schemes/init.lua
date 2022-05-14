f37c2ee14654c79b3629b20c5f9c4935386731a6
Val Subaan <rindelProgrammer2021@gmail.com>
2022-05-14 12:49:08 +0800

    edit: gruvbox font configurations

--- nvim/.config/nvim/lua/color-schemes/init.lua
+++ nvim/.config/nvim/lua/color-schemes/init.lua
@@ -1,4 +1,12 @@
 local set = vim.opt
 
+vim.cmd([[
+    let g:gruvbox_bold=1
+    let g:gruvbox_italic=1
+    let g:gruvbox_underline=1
+    let g:gruvbox_undercurl=1
+    let g:gruvbox_italicize_comments=1
+]])
+
 set.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
