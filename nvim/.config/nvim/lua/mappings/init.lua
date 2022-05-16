3154f1d4ece97ef23c5f408e5c0dbeb79b6716ee
Val Subaan <rindelProgrammer2021@gmail.com>
2022-05-16 16:10:10 +0800

    edit: mappings for neovim navigation

--- nvim/.config/nvim/lua/mappings/init.lua
+++ nvim/.config/nvim/lua/mappings/init.lua
@@ -4,12 +4,18 @@ vim.g.mapleader = ' '
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true}
 
+-- general mappings
+-- navigation
+map('n','<C-h>', '<C-w>h', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+
 -- nvim-tree mappings
 map('n', '<leader>t', ':NvimTreeToggle<CR>', opts)
 
 -- barbar mappings
 local opts = { noremap = true, silent = true }
-
 -- Move to previous/next
 map('n', '<A-,>', ':BufferPrevious<CR>', opts)
 map('n', '<A-.>', ':BufferNext<CR>', opts)
