1024ba84042e8e431980beda266e73cb21d53660
Val Subaan <rindelProgrammer2021@gmail.com>
2022-05-17 23:50:55 +0800

    edit: rename filename for toggleterm config

--- nvim/.config/nvim/lua/plugin-configs/toggleterm.lua
+++ nvim/.config/nvim/lua/plugin-configs/toggleterm.lua
@@ -2,5 +2,6 @@ require("toggleterm").setup {
     size = 15,
     open_mapping = '<F1>',
     insert_mappings = true,
-    shade_terminals = false,
+    shade_terminals = true,
+    shell = vim.o.shell,
 }
