aa6b023b34487729420692cd7f0208bd522958fa
Val Subaan <rindelProgrammer2021@gmail.com>
2022-05-17 23:43:20 +0800

    nvim plugin added: toggleterm

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,7 @@ require('color-schemes')
 require('plugin-configs.nvim-tree')
 require('plugin-configs.lualine-config')
 require('plugin-configs.barbar')
+require('plugin-configs.toggle-term')
 
 require('lsp-configs.language-servers')
 require('lsp-configs.nvim-cmp')
---
+++ nvim/.config/nvim/lua/plugin-configs/toggle-term.lua
@@ -0,0 +1,6 @@
+require("toggleterm").setup {
+    size = 15,
+    open_mapping = '<F1>',
+    insert_mappings = true,
+    shade_terminals = false,
+}
